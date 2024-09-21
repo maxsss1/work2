c315ad5549ef9ae8bc53f8f4daecdec582442732
maxsss1 <smirnov2134maxs@mail.ru>
2024-09-21 14:15:54 +0300

    1

--- ggg.php
+++ ggg.php
@@ -1,8 +1,53 @@
 <?php
-  // Define a function to print a greeting
-  function printGreeting($name) {
-    echo "Hello, " . $name . "!";
-  }
 
-  // Call the function with a name
-  printGreeting("John");
+class Книга {
+    private $title;
+    private $author;
+    private $publishedYear;
+    private $genre;
+
+    public function __construct($title, $author, $publishedYear, $genre) {
+        $this->title = $title;
+        $this->author = $author;
+        $this->publishedYear = $publishedYear;
+        $this->genre = $genre;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function getAuthor() {
+        return $this->author;
+    }
+
+    public function getPublishedYear() {
+        return $this->publishedYear;
+    }
+
+    public function getGenre() {
+        return $this->genre;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+
+    public function setAuthor($author) {
+        $this->author = $author;
+    }
+
+    public function setPublishedYear($publishedYear) {
+        $this->publishedYear = $publishedYear;
+    }
+
+    public function setGenre($genre) {
+        $this->genre = $genre;
+    }
+
+    public function getBookInfo() {
+        return "Название: " . $this->title . ", Автор: " . $this->author . ", Год публикации: " . $this->publishedYear . ", Жанр: " . $this->genre;
+    }
+}
+
+?>
--- kkk.php
+++ kkk.php
@@ -1,55 +1,4 @@
-Гладышев Сергей п33
 <?php
-
-class Книга {
-    private $title;
-    private $author;
-    private $publishedYear;
-    private $genre;
-
-    public function __construct($title, $author, $publishedYear, $genre) {
-        $this->title = $title;
-        $this->author = $author;
-        $this->publishedYear = $publishedYear;
-        $this->genre = $genre;
-    }
-
-    public function getTitle() {
-        return $this->title;
-    }
-
-    public function getAuthor() {
-        return $this->author;
-    }
-
-    public function getPublishedYear() {
-        return $this->publishedYear;
-    }
-
-    public function getGenre() {
-        return $this->genre;
-    }
-
-    public function setTitle($title) {
-        $this->title = $title;
-    }
-
-    public function setAuthor($author) {
-        $this->author = $author;
-    }
-
-    public function setPublishedYear($publishedYear) {
-        $this->publishedYear = $publishedYear;
-    }
-
-    public function setGenre($genre) {
-        $this->genre = $genre;
-    }
-
-    public function getBookInfo() {
-        return "Название: " . $this->title . ", Автор: " . $this->author . ", Год публикации: " . $this->publishedYear . ", Жанр: " . $this->genre;
-    }
-}
 
 class Библиотека {
     private $books = array();
@@ -85,40 +34,5 @@ class Библиотека {
         return $result;
     }
 }
-
-$library = new Библиотека();
-
-echo "Введите количество книг для добавления: ";
-$numBooks = trim(fgets(STDIN));
-
-for ($i = 0; $i < $numBooks; $i++) {
-    echo "Введите название книги: ";
-    $title = trim(fgets(STDIN));
-    echo "Введите автора книги: ";
-    $author = trim(fgets(STDIN));
-    echo "Введите год публикации книги: ";
-    $publishedYear = trim(fgets(STDIN));
-    echo "Введите жанр книги: ";
-    $genre = trim(fgets(STDIN));
-
-    $book = new Книга($title, $author, $publishedYear, $genre);
-    $library->addBook($book);
-}
-
-echo "Все книги: \n";
-print_r($library->listAllBooks());
-
-echo "Введите название книги для удаления: ";
-$titleToRemove = trim(fgets(STDIN));
-$library->removeBookByTitle($titleToRemove);
-
-echo "\nПосле удаления '$titleToRemove': \n";
-print_r($library->listAllBooks());
-
-echo "Введите автора для поиска книг: ";
-$authorToFind = trim(fgets(STDIN));
-$booksByAuthor = $library->findBooksByAuthor($authorToFind);
-echo "\nКниги автора $authorToFind: \n";
-print_r(array_map(function($book) { return $book->getBookInfo(); }, $booksByAuthor));
 
 ?>
